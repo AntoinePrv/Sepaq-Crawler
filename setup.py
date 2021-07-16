704cafd5f31b585e64b149c0858f2422b34cf44b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-07-15 20:41:05 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup, find_packages
+
+
+__dir__ = pathlib.Path(__file__).resolve().parent
+
+
+def get_file(file: pathlib.Path) -> str:
+    """Extract all lines from a file."""
+    with open(file, "r") as f:
+        return f.read()
+
+
+setup(
+    name="sepaq_crawler",
+    author="Antoine Prouvost",
+    description="Utility to crawl the SEPAQ API.",
+    long_description=get_file(__dir__ / "README.md"),
+    license="MIT",
+    url="https://github.com/AntoinePrv/Sepaq-Crawler",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["requests", "click", "geopy"],
+)
